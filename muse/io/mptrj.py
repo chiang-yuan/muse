8b51256f9202a1646a1c5bc0cc0539da22a13b15
Yuan Chiang <cyrusyc@berkeley.edu>
2023-09-18 22:07:24 -0700

    add binary G-x diagram

---
+++ muse/io/mptrj.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+from pymatgen.core.trajectory import Trajectory
+
+__author__ = "Yuan Chiang"
+__date__ = "2023-08-02"
+
+
+
+def pmgtraj_to_extxyz(pmgtraj: Trajectory, fname: str):
+    with open(fname, 'w') as f:
+        
+        for iframe, structure in enumerate(pmgtraj):
+            
+            # Write the extxyz format for each snapshot
+            f.write(f"{len(structure)}\n")
+            f.write(f"Lattice=\"{' '.join(map(str, structure.lattice.matrix.ravel()))}\" ")  # noqa: E501
+            f.write("Properties=species:S:1:pos:R:3")
+            
+            properties = pmgtraj.frame_properties[iframe]
+            if "forces" in properties:
+                f.write(":forces:R:3")
+            if "charges" in properties:
+                f.write(":charges:R:1")
+            if "magmoms" in properties:
+                f.write(":magmoms:R:3")
+            if "dipoles" in properties:
+                f.write(":dipoles:R:3")
+            
+            if "e_0_energy" in properties:
+                f.write(f" energy={properties['e_0_energy']}")
+            if "e_fr_energy" in properties:
+                f.write(f" free_energy={properties['e_fr_energy']}")
+            # e_wo_entrp: energy without electronic entropy (0K electron)
+            if "e_wo_entrp" in properties: 
+                f.write(f" e_wo_entrp={properties['e_wo_entrp']}")
+                
+            if "stresses" in properties:
+                f.write(f" stresses=\"{' '.join(map(str, np.array(properties['stresses']).ravel()))}\"")  # noqa: E501
+                
+            f.write("\n")
+            
+            for idx, site in enumerate(structure):
+                line = f"{site.species_string} {' '.join(map(str, site.coords))}"
+                
+                if "forces" in properties:
+                    line += f" {' '.join(map(str, properties['forces'][idx]))}"
+                if "charges" in properties:
+                    line += f" {' '.join(map(str, properties['charges'][idx]))}"
+                if "magmoms" in properties:
+                    line += f" {' '.join(map(str, properties['magmoms'][idx]))}"
+                if "dipoles" in properties:
+                    line += f" {' '.join(map(str, properties['dipoles'][idx]))}"
+                        
+                line += "\n"
+                f.write(line)
+                
---
+++ muse/plots/pd.py
@@ -0,0 +1,100 @@
+from typing import Sequence
+
+import numpy as np
+from ase import Atoms, units
+from ase.build import sort
+from ase.formula import Formula
+from matplotlib.axes._axes import Axes
+from matplotlib.figure import Figure
+
+eps = 1e-10
+
+class BinaryGXDiagram(Axes):
+    """Binary phase diagram plotter"""
+
+    def __init__(
+        self,
+        fig: Figure,
+        rect: Sequence[float],
+        *,
+        facecolor = None,
+        frameon: bool = True,
+        sharex: Axes | None = None,
+        sharey: Axes | None = None,
+        label: str = "",
+        xscale: float | None = None,
+        yscale: float | None = None,
+        box_aspect: float | None = None,
+        **kwargs
+    ) -> None:
+        super().__init__(
+            fig,
+            rect,
+            facecolor=facecolor,
+            frameon=frameon,
+            sharex=sharex,
+            sharey=sharey,
+            label=label,
+            xscale=xscale,
+            yscale=yscale,
+            box_aspect=box_aspect,
+            **kwargs
+        )
+
+    def from_trajectories(
+        self,
+        trajectories: Sequence[Sequence[Atoms]],
+        phases: Sequence[str | Formula],
+        temperature: float | None = None,
+        label: str | None = None,
+        **kwargs
+    ) -> None:
+        """Plot a binary phase diagram from a list of trajectories"""
+
+        assert len(phases) == 2
+
+        # Change strings to Formula objects and sort symbols
+        for phase in phases:
+            phase = Formula.from_list(phase) if isinstance(phase, str) else sort(phase)
+            
+
+        x, ergavgs, ergstds = [], [], []
+        for traj in trajectories:
+
+            atoms = traj[0]
+            formula = atoms.symbols.formula
+
+            portions = {}
+            for phase in phases:
+                portions[phase] = formula//phase
+
+            total_units = sum(portions.values())
+
+            x.append(portions[phases[-1]] / total_units)
+
+            energies = []
+            for atoms in traj:
+                energies.append(atoms.get_potential_energy())
+            ergavgs.append(np.mean(energies) / total_units)
+            ergstds.append(np.std(energies) / total_units)
+
+
+        x = np.array(x)
+        idx = np.argsort(x)
+        x = x[idx]
+
+        ergavgs = np.array(ergavgs)[idx]
+        ergstds = np.array(ergstds)[idx]
+
+        dH = ergavgs - (ergavgs[0] + x * (ergavgs[-1] - ergavgs[0]))
+        dS = - units.kB * (x*np.log(x + eps) + (1-x)*np.log(1-x + eps))
+
+        self.plot(x, dH, label='$\\Delta H$ ' + label if label else '$\\Delta H$', **kwargs)
+
+        if temperature is not None:
+            dG = dH - temperature * dS
+            self.plot(x, - temperature * dS, label='$-T\\Delta S$ ' + label if label else '$-T\\Delta S$', **kwargs)
+            self.plot(x, dG, label='$\\Delta G$ ' + label if label else '$\\Delta G$', **kwargs)
+        
+            
+        
