e104e07d75ca9760412b16fbb41ba9ce8c8e852c
Yuan Chiang <cyrusyc@berkeley.edu>
2023-11-12 21:23:13 -0800

    update slurm and dependencies

--- muse/jobs/slurm.py
+++ muse/jobs/slurm.py
@@ -2,25 +2,35 @@ import subprocess
 
 
 def submit_job(
-        cmd: str,
-        time: str,
-        partition: str,
-        nodes: int,
-        ntasks_per_node: int,
-        job_name: str,
-        ):
-
-    scmd = [ 
+    cmd: str,
+    time: str,
+    partition: str,
+    nodes: int,
+    ntasks_per_node: int,
+    job_name: str,
+    account: str,
+    **kwargs,
+):
+    scmd = [
         *f"""sbatch
-        --time={time} 
-        --partition={partition}
+        --time={time}
+        --account={account}
+        --qos={partition}
         --job-name={job_name}
         --output={job_name}.out
         --error={job_name}.err
         --nodes={nodes}
-        --ntasks-per-node={ntasks_per_node}"""
-        .replace("'", "").split(),
-        "--wrap", cmd
+        --ntasks-per-node={ntasks_per_node}""".replace(
+            "'", ""
+        ).split(),
     ]
+
+    if kwargs.get("constraint", None):
+        scmd += [f"--constraint={kwargs['constraint']}"]
+
+    if kwargs.get("exclude", None):
+        scmd += ["--exclude"]
+
+    scmd += ["--wrap", cmd]
 
     return subprocess.run(scmd, check=True)
