47a96da1caede92e076c91756a3611ea4f9188ed
Yuan Chiang <cyrusyc@berkeley.edu>
2023-12-07 17:50:28 -0800

    add density option to specify desired density

--- muse/transforms/mixture.py
+++ muse/transforms/mixture.py
@@ -1,6 +1,7 @@
 import numpy as np
 from ase import Atoms
 from ase.build import sort
+from ase.cell import Cell
 from ase.formula import Formula
 from ase.io import read
 from monty.tempfile import ScratchDir
@@ -11,22 +12,38 @@ from pymatgen.io.packmol import PackmolBoxGen
 
 from muse.utils import MP_API_KEY
 
-mpr = MPRester(MP_API_KEY)
-
 
 def mix(
     recipe: dict[str, int],
-    tolerance=2.0,
-    rattle=0.5,
-    scale=1.0,
-    shuffle=True,
-    seed=1,
-    log=False,
+    density: float | None = None,
+    tolerance: float = 2.0,
+    rattle: float = 0.5,
+    scale: float = 1.0,
+    shuffle: bool = True,
+    seed: int = 1,
+    log: bool = False,
+    mp_api_key: str = MP_API_KEY,
 ) -> Atoms:
-    """
-    Mixes a set of molecules according to a recipe.
+    """Mixes a set of molecules according to a recipe.
+
+    Args:
+        recipe (dict[str, int]): a dictionary of molecules and their desired ratios.
+        density (float | None, optional): assinged mass density in atomic units.
+            Defaults to None to calculate from solid state 0K density.
+        tolerance (float, optional): tolerance for packmol in Angstrom. Defaults to 2.0.
+        rattle (float, optional): position rattle in Angstrom. Defaults to 0.5.
+        scale (float, optional): scale factor for box size. Defaults to 1.0.
+        shuffle (bool, optional): shuffle atomic species. Defaults to True.
+            Otherwise, the neighbor environment will be similar to solid state.
+        seed (int, optional): Defaults to 1.
+        log (bool, optional): Defaults to False.
+        mp_api_key (str, optional): Defaults to MP_API_KEY.
+
+    Returns:
+        Atoms: generated structure as ASE Atoms object.
     """
 
+    mpr = MPRester(MP_API_KEY)
     np.random.seed(seed)
 
     molecules = []
@@ -160,5 +177,25 @@ def mix(
 
     if shuffle:
         atoms.numbers = np.random.permutation(atoms.numbers)
+
+    if density is not None:
+        cellpar = atoms.cell.cellpar()
+
+        volume = atoms.get_masses().sum() / density
+
+        factor = (volume / atoms.get_volume()) ** (1.0 / 3.0)
+        atoms.set_cell(
+            Cell.fromcellpar(
+                [
+                    cellpar[0] * factor,
+                    cellpar[1] * factor,
+                    cellpar[2] * factor,
+                    cellpar[3],
+                    cellpar[4],
+                    cellpar[5],
+                ]
+            ),
+            scale_atoms=True,
+        )
 
     return sort(atoms)
